c19e44e673e79a098d2c9fc73a0e680b19b2300d
Seth Thompson <thompsonrussell22@gmail.com>
2024-08-30 15:29:37 -0400

    added basic navigation bar

---
+++ src/app/components/NavigationBar.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { useState } from "react";
+import styles from "./NavigationBar.module.css";
+
+export function NavigationBar() {
+//   const [showMenu, setShowMenu] = useState(false);
+//   const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setShowMenu(!showMenu);
+//     setIsSideNavOpen(!isSideNavOpen);
+//   };
+
+  return (
+    <div className="nav">
+      <ul className={styles.navbar} id="navbar">
+        <div className={styles.leftlinks}>
+          <li className={styles.link}>
+            <a className={styles["icontext navitem"]} href="/">
+              Home Page
+            </a>
+          </li>
+        </div>
+        {/* <div className={styles[`rightlinks ${showMenu ? "showmenu" : ""}`]}> */}
+        <div className={styles["rightlinks showmenu"]}>
+          <li className={styles.link}>
+            <a className={`${styles.invertedbtn} ${styles.navitem}`} href="/">
+              Sign Up
+            </a>
+          </li>
+          <li className={styles.link}>
+            <a className={`${styles.normalbtn} ${styles.navitem}`} href="/">
+              Login
+            </a>
+          </li>
+        </div>
+        { /*
+        <li className={styles["menubutton"]} onClick={toggleMenu}>
+          <div className={styles[`hamburgericon ${isSideNavOpen ? "open" : ""}`]}>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+        </li> */}
+      </ul> 
+      {/* <div className={styles[`sidenavigation ${showMenu ? "show" : ""}`]}>
+        <ul className={styles["sidebarcontent"]}>
+          <li>
+            <a className="" href="/">
+              Sign Up
+            </a>
+          </li>
+          <li>
+            <a className="" href="/">
+              Login
+            </a>
+          </li>
+        </ul>
+      </div> */}
+    </div>
+  );
+}
+
+export default NavigationBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import NavigationBar from "./components/NavigationBar";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <NavigationBar/>
+      <div>
         Hello World
       </div>
     </main>
